1e623a3ef4637683d43f7f2abd2584c35cf7d19a
Morganne Maziere <morganne.maziere@kedgebs.com>
2023-06-27 16:20:53 +0200

    controller

--- app/controllers/buddies_controller.rb
+++ app/controllers/buddies_controller.rb
@@ -1,5 +1,5 @@
 class BuddiesController < ApplicationController
-  before_action :set_buddy, only: %i[show destroy update]
+  before_action :set_buddy, only: %i[show edit destroy update]
 
   def index
     @buddies = User.all
@@ -9,18 +9,41 @@ class BuddiesController < ApplicationController
     @new_buddy = BuddiesList.new
   end
 
+  def new
+    @buddy = Buddy.new
+  end
+
   def create
-
+    @buddy = Buddy.new(buddy_params)
+    if @buddy.save
+      redirect_to @buddy, notice: 'Buddy was successfully created.'
+    else
+      render :new
+    end
   end
 
-  def edit
+  def edit; end
 
+  def update
+    if @buddy.update(buddy_params)
+      redirect_to @buddy, notice: 'Buddy was successfully updated.'
+    else
+      render :edit
+    end
   end
 
+  def destroy
+    @buddy.destroy
+    redirect_to buddies_url, notice: 'Buddy was successfully destroyed.'
+  end
 
   private
 
   def set_buddy
     @buddy = User.find(params[:id])
+  end
+
+  def buddy_params
+    params.require(:buddy).permit(:user_id, :buddy_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get "buddies", to: "buddies#index"
   get "buddies/:id", to: "buddies#show", as: :buddy
   post "buddies/:id", to: "buddies#create", as: :create_buddy
+
+  get "events", to: "events#index"
+  get "events/:id", to: "events#show", as: :new_event
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
