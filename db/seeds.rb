0444673671e42abf275d5ca0a92fdbe6dd1584ea
Charlotte Salmona <charlotte.salmona@kedgebs.com>
2023-06-28 16:26:14 +0200

    show et index vrmt a bout

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,16 +9,17 @@
 Sport.destroy_all
 Event.destroy_all
 Booking.destroy_all
+UserSport.destroy_all
 BuddiesList.destroy_all
 User.destroy_all
 
 # user
 puts 'user creation'
-morganne = User.create!(email: 'momo@gmail.com', password: 'morganne', first_name: "Morganne", last_name: "Maziere", profilepic: "morganne.jpeg", description: "Salut, j'adore le sport et partager des moments aves mes amis!" )
+morganne = User.create!(email: 'momo@gmail.com', password: 'morganne', first_name: "Morganne", last_name: "Maziere", profilepic: "morganne.jpeg", description: "Salut, j'adore le sport et partager des moments aves mes amis!", location: "Marseille" )
 
-charlotte = User.create!(email: 'cha@gmail.com', password: 'charlotte', first_name: "Charlotte", last_name: "Salmona", profilepic: "charlotte.jpeg", description: "Salut, j'adore le sport et partager des moments aves mes amis!")
+charlotte = User.create!(email: 'cha@gmail.com', password: 'charlotte', first_name: "Charlotte", last_name: "Salmona", profilepic: "charlotte.jpeg", description: "Salut, j'adore le sport et partager des moments aves mes amis!", location: "Marseille")
 
-anouk = User.create!(email: 'noukita@gmail.com', password: 'anouk1', first_name: "Anouk", last_name: "Semerdjian", profilepic: "anouk.jpeg", description: "Salut, j'adore le sport et partager des moments aves mes amis!")
+anouk = User.create!(email: 'noukita@gmail.com', password: 'anouk1', first_name: "Anouk", last_name: "Semerdjian", profilepic: "anouk.jpeg", description: "Salut, j'adore le sport et partager des moments aves mes amis!", location: "Marseille")
 puts 'finished!'
 
 
@@ -27,23 +28,36 @@ puts 'buddy creation'
 buddyList1 = BuddiesList.create!(user: morganne, buddy: charlotte)
 buddyList2 = BuddiesList.create!(user: morganne, buddy: anouk)
 buddyList3 = BuddiesList.create!(user: charlotte, buddy: anouk)
+buddyList3 = BuddiesList.create!(user: charlotte, buddy: morganne)
 # buddylist1 = morganne.BuddiesList.create!(buddy: charlotte)
 # buddylist2 = morganne.BuddiesList.create!(buddy: anouk)
 # buddylist3 = charlotte.BuddiesList.create!(buddy: anouk)
 puts 'finished!'
 
+
 # sport
 puts 'sport creation'
 yoga = Sport.create!(name: 'Yoga', level: 'Débutant',
-  description: "Le yoga est une pratique holistique qui combine des postures physiques,
-  des exercices de respiration et la méditation pour favoriser la flexibilité, la force
-  et la paix intérieure.")
+                    description: "Le yoga est une pratique holistique qui combine des postures physiques,
+                    des exercices de respiration et la méditation pour favoriser la flexibilité, la force
+                    et la paix intérieure.")
 
 jump = Sport.create!(name: 'Jump', level: 'Confirmé',
-  description: "Jump est un sport fitness à haute intensité qui implique différents
-  mouvements explosifs, tels que des sauts, des squats et des fentes, conçu pour améliorer
-  l'endurance cardiovasculaire et la force musculaire..")
+                    description: "Jump est un sport fitness à haute intensité qui implique différents
+                    mouvements explosifs, tels que des sauts, des squats et des fentes, conçu pour améliorer
+                    l'endurance cardiovasculaire et la force musculaire..")
 puts 'finished!'
+
+
+# user sports
+puts 'users sports creation'
+UserSport.create!(user: anouk, sport: yoga)
+UserSport.create!(user: charlotte, sport: yoga)
+UserSport.create!(user: charlotte, sport: jump)
+UserSport.create!(user: morganne, sport: jump)
+UserSport.create!(user: morganne, sport: yoga)
+puts 'finished!'
+
 
 # event
 puts 'event creation'
